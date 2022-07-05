3c21e0025f220739642eee747cefc1ef99699cf2
Assad Isah <nottherealalanturing@gmail.com>
2022-07-05 16:14:28 +0100

    feat: create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,3 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals(id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR, date_of_birth VARCHAR, escape_attempts VARCHAR, neutered BOOL, weight_kg BOOL);

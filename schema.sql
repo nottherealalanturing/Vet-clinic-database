bdcdb896bbbf7175144864074a2f8bf809afb24f
Assad Isah <nottherealalanturing@gmail.com>
2022-07-05 16:32:17 +0100

    fix: drop table and re create to fix wrong data type

--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals(id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR, date_of_birth DATE, escape_attempts VARCHAR, neutered BOOL, weight_kg BOOL);
+CREATE TABLE animals(id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR, date_of_birth DATE, escape_attempts INT, neutered BOOL, weight_kg DECIMAL);
